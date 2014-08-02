489f2682959e2413305cdc082d92917b7ff7a9b5
Meenalkumar Valhekar <mvalhekar@spiderlogic.com>
2014-08-03 01:21:32 +0530

    added UQ for emp_emailId

--- root/code/LateComerTracker/TrackerMigrator/20140802194036_CreateEmployeeTable.cs
+++ root/code/LateComerTracker/TrackerMigrator/20140802194036_CreateEmployeeTable.cs
@@ -11,6 +11,7 @@
                                        + ",[emp_name] [nvarchar](20) NOT NULL"
                                        + ",[emp_emailId] [nvarchar](50) NOT NULL"
                                        + ",CONSTRAINT PK_Employee_Id PRIMARY KEY (emp_id)"
+                                       + ",CONSTRAINT UK_Employee_emailId UNIQUE (emp_emailId)"
                                        + ")";
 
             ExecuteNonQuery(commantText);
